08575c9864b147121a6975839e8d7dca592e77bc
aidkvndfj <ericm7597@gmail.com>
2024-11-17 21:44:17 -0500

    changed app name

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module HelloApp
+module WebDropDead
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.web_console.permissions = '192.168.2.70' if Rails.env.development?
+    # config.web_console.permissions = '192.168.2.70' if Rails.env.development?
 
     # Configuration for the application, engines, and railties goes here.
     #
